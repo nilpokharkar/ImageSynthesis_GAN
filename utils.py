34a1d93e025052b2e7296074d97268a19d04ecd6
Nilakshi Pokharkar <30683139+nilpokharkar@users.noreply.github.com>
2023-04-17 21:14:14 -0400

    Helper file

---
+++ utils.py
@@ -0,0 +1,45 @@
+import errno
+import os
+import shutil
+import sys
+
+
+def to_np(var):
+    return var.detach().cpu().numpy()
+
+
+def create_folder(folder_path):
+    try:
+        os.makedirs(folder_path)
+    except OSError as _e:
+        if _e.errno != errno.EEXIST:
+            raise
+
+
+def clear_folder(folder_path):
+    create_folder(folder_path)
+    for the_file in os.listdir(folder_path):
+        _file_path = os.path.join(folder_path, the_file)
+        try:
+            if os.path.isfile(_file_path):
+                os.unlink(_file_path)
+            elif os.path.isdir(_file_path):
+                shutil.rmtree(_file_path)
+        except OSError as _e:
+            print(_e)
+
+
+class StdOut(object):
+    def __init__(self, output_file):
+        self.terminal = sys.stdout
+        self.log = open(output_file, "a")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.terminal.flush()
+        self.log.write(message)
+        self.log.flush()
+
+    def flush(self):
+        self.terminal.flush()
+        self.log.flush()
